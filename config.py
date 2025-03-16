340df0de1cdf7161a2d99f9429d73d7aff622cad
Loka Siva Ganesh Reddy Vajrala <138133797+Ganesh-Vajrala@users.noreply.github.com>
2025-03-16 23:12:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,1 @@
-API_KEYS = {
-    "amazon": "a204b97022msh49a0f95d0750297p11c003jsnb4586b8fa670",
-    "flipkart": "a204b97022msh49a0f95d0750297p11c003jsnb4586b8fa670",
-    "instagram": "a8ce2ea652mshde3a718ad1aad98p1aced7jsn5740eb820bfd",
-    "amazon_review":"67cb12a777169277762894ab"
-}
 
-DEFAULT_MODEL = "SVM"
